72ba8c1425b0d6ec05124395acc67ee0d565f6f1
minimamujiro <minimamujiro1650@gmail.com>
2026-02-26 13:38:24 +0900

    Implement advanced login

---
+++ db/migrate/20260222030647_add_remenber_digest_to_users.rb
@@ -0,0 +1,5 @@
+class AddRemenberDigestToUsers < ActiveRecord::Migration[8.0]
+  def change
+    add_column :users, :remember_digest, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_08_120301) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_22_030647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_02_08_120301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
